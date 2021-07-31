91156771bf2bf68b868a30287b863cc8b216ca66
Carlos <carlosferreira@alu.ufc.br>
2021-07-31 18:48:34 -0300

    Setup do projeto e Design do layout das telas de Sign Up e Sign In

---
+++ app/src/main/java/com/br/chatter/activities/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.br.chatter.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.br.chatter.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
