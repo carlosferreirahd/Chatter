783fc139fc7e284b5c44967c6f88aacbef158c2c
Carlos <carlosferreira@alu.ufc.br>
2021-08-01 16:52:28 -0300

    Display da lista de usuários do Firestore Database

---
+++ app/src/main/java/com/br/chatter/models/User.java
@@ -0,0 +1,10 @@
+package com.br.chatter.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name;
+    public String email;
+    public String image;
+    public String token;
+}
