3e2c5b33226f1190eb80f91efa6578c500100598
Carlos <carlosferreira@alu.ufc.br>
2021-08-01 11:06:15 -0300

    Sign In usando Firestore Database e Shared Preferences

--- app/src/main/java/com/br/chatter/activities/SignInActivity.java
+++ app/src/main/java/com/br/chatter/activities/SignInActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.br.chatter.databinding.ActivitySignInBinding;
+import com.br.chatter.utils.Constants;
+import com.br.chatter.utils.PreferenceManager;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -22,5 +36,70 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> {
+            if(isValidSignInDetails()) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        loading(true);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                   if(task.isSuccessful() && task.getResult() != null
+                           && task.getResult().getDocuments().size() > 0) {
+                       DocumentSnapshot snapshot = task.getResult().getDocuments().get(0);
+
+                       preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                       preferenceManager.putString(Constants.KEY_USER_ID, snapshot.getId());
+                       preferenceManager.putString(Constants.KEY_NAME, snapshot.getString(Constants.KEY_NAME));
+                       preferenceManager.putString(Constants.KEY_IMAGE, snapshot.getString(Constants.KEY_IMAGE));
+
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                       startActivity(intent);
+                   } else {
+                       loading(false);
+                       showToast("Oops!... não foi possível fazer login, tente novamente!");
+                   }
+                });
+    }
+
+    private void loading(Boolean isLoading) {
+        if(isLoading) {
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidSignInDetails() {
+        if(binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Digite seu email!");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Digite um email válido!");
+            return false;
+        }
+        else if(binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Digite sua senha!");
+            return false;
+        }
+
+        return true;
     }
 }
